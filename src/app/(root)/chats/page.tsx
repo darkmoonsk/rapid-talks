ad38c2d6ad7578cd3d13b432de4bea78f4b87e04
Bruno Souza <brunoezully@gmail.com>
2024-02-08 18:46:38 -0300

    feat: new `Chats` page layout, add `ChatList` and `Contacts` components

--- src/app/(root)/chats/page.tsx
+++ src/app/(root)/chats/page.tsx
@@ -1,13 +1,18 @@
 "use client";
-import { useSession } from 'next-auth/react';
-import React from 'react'
+import ChatList from "@/components/Chats/ChatList";
+import Contacts from "@/components/Chats/Contacts";
 
 function ChatsPage() {
-  const { data: session } = useSession();
-  console.log(session);
   return (
-    <div>Chats</div>
-  )
+    <main className="h-screen w-full flex justify-around gap-5 px-10 py-3 max-lg:gap-8">
+      <div className="w-1/3 max-md:w-full max-lg:w-1/2">
+        <ChatList />
+      </div>
+      <div className="w-[750px] max-md:hidden max-lg:w-1/2 ">
+        <Contacts />
+      </div>
+    </main>
+  );
 }
 
 export default ChatsPage;
