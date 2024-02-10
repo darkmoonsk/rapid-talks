b9fe9e4e7d653de6c6d264172fb698bbaeae65ee
Bruno Souza <brunoezully@gmail.com>
2024-02-10 01:17:47 -0300

    feat: add message population and implement marking messages as seen

--- src/app/api/chats/[chatId]/route.ts
+++ src/app/api/chats/[chatId]/route.ts
@@ -1,5 +1,6 @@
 import { connectToDB } from "@/database";
 import Chat from "@/models/Chat";
+import Message from "@/models/Message";
 import User from "@/models/User";
 
 interface Params {
@@ -15,10 +16,39 @@ export async function GET(req: Request, { params }: Params) {
     const chat = await Chat.findById(chatId).populate({
       path: "members",
       model: User
+    }).populate({
+      path: "messages",
+      model: Message,
+      populate: {
+        path: "sender seenBy",
+        model: User
+      }
     }).exec();
 
     return new Response(JSON.stringify(chat), { status: 200 });
   } catch (error) {
     return new Response("Failed to get chat details", { status: 500});
+  }
+}
+
+export async function POST(req: Request, { params }: Params) {
+  try {
+    await connectToDB();
+
+    const { chatId } = params;
+
+    const { currentUserId } = await req.json();
+
+    await Message.updateMany(
+      { chat: chatId },
+      { $addToSet: { seenBy: currentUserId } },
+    ).populate({
+      path: "sender seenBy",
+      model: User
+    }).exec();
+
+    return new Response("Seen all messages by current user", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to mark messages as seen", { status: 500});
   }
 }
