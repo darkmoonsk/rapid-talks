246c99bc0bc0c2af3717c91212d8942e9fbbb540
Bruno Souza <brunoezully@gmail.com>
2024-02-10 01:03:57 -0300

    build: add Access-Control headers for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: process.env.ALLOW_ORIGIN},
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: ""
-      }
+      },
     ],
   },
 };
 
 export default nextConfig;
-
