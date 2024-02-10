1f25da3aacb3e2296634b2738ec01a86851845ae
Bruno Souza <brunoezully@gmail.com>
2024-02-10 01:20:01 -0300

    feat: add useRef hook and implement sending text and photo messages

--- src/components/Chats/ChatDetails.tsx
+++ src/components/Chats/ChatDetails.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import IUser from "@/models/interfaces/IUser";
 import { useSession } from "next-auth/react";
@@ -8,30 +8,30 @@ import Link from "next/link";
 import Image from "next/image";
 import { AddPhotoAlternate } from "@mui/icons-material";
 import Loader from "../UI/Loader/Loader";
+import { CldUploadButton } from "next-cloudinary";
+import MessageBox from "../Message/MessageBox";
+import { pusherClient } from "@/lib/pusher";
+import IMessage from "@/models/interfaces/IMessage";
 
-interface ChatDetailsProps {
-  chatId: string | string[];
-}
-
-function ChatDetails({ chatId }: ChatDetailsProps) {
+function ChatDetails({ chatId }: any) {
   const { data: session } = useSession();
   const currentUser = session?.user as IUser;
 
   const [loading, setLoading] = useState(true);
-  const [chat, setChat] = useState<IChat>();
+  const [chat, setChat] = useState({} as IChat);
   const [otherMembers, setOtherMembers] = useState<IUser[]>();
   const [text, setText] = useState("");
 
   const getChatDetails = async () => {
     try {
       setLoading(true);
-      console.log(chatId);
       const res = await axios.get(`/api/chats/${chatId}`);
       const data = res.data;
       setChat(data);
-      setOtherMembers(
-        data.members.filter((member: IUser) => member._id !== currentUser._id),
-      );
+
+      const members  =  data?.members?.filter((member: IUser) => member._id !== currentUser._id);
+      setOtherMembers((prevMembers) => members);
+      
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -42,10 +42,68 @@ function ChatDetails({ chatId }: ChatDetailsProps) {
   useEffect(() => {
     if (currentUser) {
       getChatDetails();
+      
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser]);
+
+  const sendText = async () => {
+    try {
+      const res = await axios.post("/api/messages", {
+        chatId,
+        currentUserId: currentUser._id,
+        text,
+      });
+
+      if(res.status === 201) {
+        setText("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendPhoto = async (result: any) => {
+    try {
+      const res = await axios.post("/api/messages", {
+        chatId,
+        currentUserId: currentUser._id,
+        photo: result?.info?.secure_url,
+      });
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    pusherClient.subscribe(chatId);
+
+    const handleMessage = async (newMessage: any) => {
+      setChat((prevChat) => {
+        return {
+          ...prevChat,
+          messages: [...prevChat.messages, newMessage],
+        };
+      });
+    };
+
+    pusherClient.bind("new-message", handleMessage);
+    
+    return () => {
+      pusherClient.unsubscribe(chatId);
+      pusherClient.unbind("new-message", handleMessage);
+    };
+  }, [chatId]);
+
+  // Movendo até a ultima/nova mensagem
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chat?.messages])
+  
 
   return (
     <div className="w-full h-full flex flex-col bg-white rounded-2xl">
@@ -84,17 +142,36 @@ function ChatDetails({ chatId }: ChatDetailsProps) {
           )}
         </div>
       )}
-      <div className="flex-1 flex flex-col gap-5 bg-grey-2 p-5 no-scrollbar overflow-y-scroll"></div>
+      
+      {/*  Messagens do chat  */}
+      <div className="flex-1 flex flex-col gap-5 bg-grey-2 p-5 no-scrollbar overflow-y-scroll">
+        {chat?.messages?.map((message, index) => (
+          <MessageBox
+            key={index}
+            message={message}
+            currentUser={currentUser}
+          />
+        ))}
+        
+        <div ref={bottomRef} />
+      </div>
+
       <div className="w-full flex items-center justify-between px-7 py-3 rounded-3xl cursor-pointer bg-white">
         <div className="flex items-center w-full gap-4">
-          <AddPhotoAlternate
-            sx={{
-              fontSize: "35px",
-              color: "#535353",
-              cursor: "pointer",
-              "&:hover": { color: "pink" },
-            }}
-          />
+          <CldUploadButton
+            options={{ maxFiles: 1 }}
+            onUpload={sendPhoto}
+            uploadPreset="lkrqrnff"
+          >
+            <AddPhotoAlternate
+              sx={{
+                fontSize: "35px",
+                color: "#535353",
+                cursor: "pointer",
+                "&:hover": { color: "pink" },
+              }}
+            />
+          </CldUploadButton>
           <input
             type="text"
             placeholder="Digite uma mensagem"
@@ -105,11 +182,14 @@ function ChatDetails({ chatId }: ChatDetailsProps) {
         </div>
         <div>
           <Image 
+            onClick={sendText}
             src="/images/send.jpg"
             alt="Enviar mensagem"
             width="40"
             height="40"
-            className="w-[40px] h-[40px] rounded-full object-cover object-center"
+            className="w-[40px] h-[40px] rounded-full object-cover object-center
+              hover:scale-125 ease-in-out duration-300 cursor-pointer
+            "
           />
         </div>
       </div>
