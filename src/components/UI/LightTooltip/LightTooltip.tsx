863c1a484e44e723eb66f3f1822d2b6e94c95400
Bruno Souza <brunoezully@gmail.com>
2024-02-11 01:51:58 -0300

    feat: add scrollbar styling and create `LightTooltip` and `LogoutButton` components

---
+++ src/components/UI/LightTooltip/LightTooltip.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+
+
+const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}));
+
+export default LightTooltip;
