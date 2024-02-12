cbb9d1f37b7cc6a802e12d6e54e2b7a3c1a982d4
Bruno Souza <brunoezully@gmail.com>
2024-02-12 00:08:08 -0300

    fix: `search-chat` route to populate messages

--- src/app/api/users/[userId]/search-chat/[query]/route.ts
+++ src/app/api/users/[userId]/search-chat/[query]/route.ts
@@ -1,19 +1,28 @@
 import { connectToDB } from "@/database";
 import Chat  from "@/models/Chat";
+import Message from "@/models/Message";
 import User from "@/models/User";
+import type { NextApiRequest } from "next";
 
-export async function GET (req: Request, { params }: any) {
+export async function GET (req: NextApiRequest, { params }: any) {
   try {
     await connectToDB();
 
     const { userId, query } = params;
-
+    
     const searchedChat = await Chat.find({
       members: userId,
       name: { $regex: query, $options: "i" }
     }).populate({
       path: "members",
       model: User
+    }).populate({
+      path: "messages",
+      model: Message,
+      populate: {
+        path: "sender seenBy",
+        model: User,
+      }
     }).exec();
 
     return new Response(JSON.stringify(searchedChat), { status: 200 });
